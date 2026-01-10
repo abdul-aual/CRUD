f3a9834617ee339fa9db5e5b77f5ca5b5dae99f3
abdul-aual <mdabdulaual38@gmail.com>
2026-01-11 01:07:33 +0600

    cleaning

--- src/express.ts
+++ src/express.ts
@@ -1,83 +1,23 @@
 import express, { type Request, type Response } from 'express';
-import {Pool} from "pg";
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({path:path.join(process.cwd(), ".env")})
+import config from './config';
+import { pool } from './config/db';
+import initDB from './config/db';
+import logger from './middleware/logger';
+import { userRoutes } from './modules/user/user.routes';
 
 const app = express();
-const port = 3000;
+const port = config.port;
+
 //parser
 app.use(express.json());
 
-const pool = new Pool({
-    connectionString: `${process.env.connection_str}`
-});
-
-const initDB = async()=>{
- await pool.query(`
-    CREATE TABLE IF NOT EXISTS users(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    age INT,
-    phone VARCHAR(15),
-    address TEXT,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-    )
-    `);
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS todos(
-        id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES users(id) ON DELETE CASCADE,
-        title VARCHAR(200) NOT NULL,
-        description TEXT,
-        due_date DATE,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )
-        `)
-}
 initDB();
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', logger, (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-
-app.post("/users", async(req:Request, res:Response)=>{
-    const {name, email} = req.body;
-    try{
-        const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2)`, [name, email]);
-        res.status(201).json({
-            success:true,
-            message:"Data inserted successfully"
-           // data: result.rows[0]
-        })
-    }catch(err:any){
-        res.status(500).json({
-            success: false,
-            message: err.message
-        })
-    }
-})
-
-//all users
-app.get("/users", async(req:Request, res:Response)=>{
-    try{
-        const result = await pool.query(`SELECT * FROM users`);
-        res.status(200).json({
-            success:true,
-            message:"users retrived successfully",
-            data: result.rows
-        })
-    }catch(err:any){
-        res.status(500).json({
-            success: false,
-            message: err.message
-        })
-    }
-});
+app.use("/users", userRoutes);
 
 //single user
 app.get("/users/:id", async(req:Request, res:Response)=>{
